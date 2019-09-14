b77ac5469ea49dae5754791bcb561335306962cd
duan <naonao800@163.com>
2019-09-15 03:01:26 +0800

    添加vue脚手架

---
+++ src/views/home.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<h1>list s3 music</h1>
+		<amplify-authenticator></amplify-authenticator>
+	</div>
+</template>
+
+<script>
+// import { AmplifyEventBus } from 'aws-amplify-vue';
+
+export default {
+	name: 'home',
+	data () {
+		return {
+			userinfo: {
+				username: '',
+				password: '',
+			}
+		};
+	},
+	created () {
+	},
+}
+</script>
